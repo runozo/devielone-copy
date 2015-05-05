08a3b42303cdf42e8e6024e89dbf7e9cab0ceee4
Cesare Ghirelli <cghirelli@arpa.emr.it>
2015-05-05 11:23:01 +0200

    Added some usage instructions

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # devielone-copy
+
+Copies a list of files listed in a csv file, from a source directory to a destination directory.
+
+## Usage
+
+devielone_copy.py [-h] [-N N] [-S S] [-E E] csv_file source_path destination_path
+
+positional arguments:
+  csv_file          a csv file with a list of file names to be copied
+  source_path       the source path to copy the files
+  destination_path  the destination path to copy the files
+
+optional arguments:
+  -h, --help        show this help message and exit
+  -N N              the number of the column where the file names are stored,
+                    default: "0"
+  -S S              the separator for the columns of the csv file, default: ";"
+  -E E              the file extension
--- devielone_copy.py
+++ devielone_copy.py
@@ -4,13 +4,15 @@ import argparse
 from shutil import copyfile
 from os import path
 
-parser = argparse.ArgumentParser(description='Process some integers.')
+parser = argparse.ArgumentParser(description='Copies files listed in a csv file.')
 parser.add_argument(
     'csv_file', type=str, help='a csv file with a list of file names to be copied')
 parser.add_argument(
     '-N', type=int, help='the number of the column where the file names are stored, default: 0', default=0, required=False)
 parser.add_argument(
-    '-S', type=str, help='the separator for the columns of the csv file, default: ;', default=';', required=False)
+    '-S', type=str, help='the separator for the columns of the csv file, default: ";"', default=';', required=False)
+parser.add_argument(
+    '-E', type=str, help='the source files extension, default: "pdf"', default='pdf', required=False)
 parser.add_argument(
     'source_path', type=str, help='the source path to copy the files')
 parser.add_argument(
@@ -21,8 +23,8 @@ args = parser.parse_args()
 
 with open(args.csv_file, 'r') as csv:
     for line in csv:
-        filename = line.split(args.S)[args.N]
+        filename = line.split(args.S)[args.N] + '.' + args.E
         print('Copying %s to %s', (path.join(args.source_path, filename),
                                    path.join(args.destination_path, filename)))
         copyfile(path.join(args.source_path, filename),
-                 copyfile(path.join(args.destination_path, filename)))
+                 path.join(args.destination_path, filename))
